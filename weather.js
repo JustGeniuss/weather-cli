aa754cc4c88477112ddcea64bfec61a8406fff08
JustGeniuss <danila.kukushkin98@gmail.com>
2023-01-31 05:41:39 +0300

    add log and storage service

--- weather.js
+++ weather.js
@@ -1,19 +1,39 @@
 #!usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан токен');
+        return;
+    }
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Токен сохранен');
+    } catch (e) {
+        printError(e.message);
+    }
+};
+
+
+
+
 
 const initCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
-        // вывод help
+        printHelp();
     }
     if (args.s) {
         // Сохранить город
     }
-    if (args.h) {
-        // Сохранить токен
+    if (args.t) {
+        return saveToken(args.t)
     }
     // Вывести токен
- 
+
     console.log(args);
 };
 
